32ba8ee4fd7facb477fa3336152bf92df7e96d8e
sam <evaoosyz@gmail.com>
2021-11-16 22:19:46 +0800

    加入 IEnumerableExtensions 的 JoinString

---
+++ YameTools/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+
+namespace YameTools.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static string JoinString<T>(this IEnumerable<T> source, string seperator) =>
+            string.Join(seperator, source.Select(x => x.ToString()));
+
+        public static string JoinString(this IEnumerable<string> source, string seperator) =>
+            string.Join(seperator, source);
+    }
+}
