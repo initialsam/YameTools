c40221d4f48b529b6273d8fa3911f8a2000d3095
sam <evaoosyz@gmail.com>
2022-05-26 23:09:11 +0800

    IQueryableExtension Add FilterByItems

---
+++ YameTools/Extensions/ExpressionReplacer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+using System;
+
+namespace YameTools.Extensions
+{
+    public class ExpressionReplacer : ExpressionVisitor
+    {
+        readonly IDictionary<Expression, Expression> _replaceMap;
+
+        public ExpressionReplacer(IDictionary<Expression, Expression> replaceMap)
+        {
+            _replaceMap = replaceMap ?? throw new ArgumentNullException(nameof(replaceMap));
+        }
+
+        public override Expression Visit(Expression exp)
+        {
+            if (exp != null && _replaceMap.TryGetValue(exp, out var replacement))
+                return replacement;
+            return base.Visit(exp);
+        }
+
+        public static Expression Replace(Expression expr, Expression toReplace, Expression toExpr)
+        {
+            return new ExpressionReplacer(new Dictionary<Expression, Expression> { { toReplace, toExpr } }).Visit(expr);
+        }
+
+        public static Expression Replace(Expression expr, IDictionary<Expression, Expression> replaceMap)
+        {
+            return new ExpressionReplacer(replaceMap).Visit(expr);
+        }
+
+        public static Expression GetBody(LambdaExpression lambda, params Expression[] toReplace)
+        {
+            if (lambda.Parameters.Count != toReplace.Length)
+                throw new InvalidOperationException();
+
+            return new ExpressionReplacer(Enumerable.Range(0, lambda.Parameters.Count)
+                .ToDictionary(i => (Expression)lambda.Parameters[i], i => toReplace[i])).Visit(lambda.Body);
+        }
+    }
+}
--- YameTools/Extensions/IQueryableExtension.cs
+++ YameTools/Extensions/IQueryableExtension.cs
@@ -42,5 +42,49 @@ namespace YameTools.Extensions
 
 
         }
+        /// <summary>
+        ///
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TItem"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="items"></param>
+        /// <param name="filterPattern"></param>
+        /// <param name="isOr"></param>
+        /// <returns></returns>
+        public static IQueryable<T> FilterByItems<T, TItem>(
+            this IQueryable<T> query,
+            IEnumerable<TItem> items,
+            Expression<Func<T, TItem, bool>> filterPattern,
+            bool isOr)
+        {
+            Expression predicate = null;
+            foreach (var item in items)
+            {
+                var itemExpr = Expression.Constant(item);
+                var itemCondition = ExpressionReplacer.Replace(filterPattern.Body, filterPattern.Parameters[1], itemExpr);
+                if (predicate == null)
+                    predicate = itemCondition;
+                else
+                {
+                    predicate = Expression.MakeBinary(isOr ? ExpressionType.OrElse : ExpressionType.AndAlso, predicate,
+                        itemCondition);
+                }
+            }
+
+            if (predicate is null)
+            {
+                predicate = Expression.Constant(false);
+            }
+
+            var filterLambda = Expression.Lambda<Func<T, bool>>(predicate, filterPattern.Parameters[0]);
+
+            return query.Where(filterLambda);
+
+            /// items = items.FilterByItems(
+            ///     KeyWordList,
+            ///     (x, keyWord) => x.DemoA.Contains(keyWord) || x.DemoB.Contains(keyWord),
+            ///     true);
+        }
     }
 }
