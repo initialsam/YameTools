5352bf6cd2122f747806fbae11225bc497ba5cf2
sam <evaoosyz@gmail.com>
2022-06-04 12:14:37 +0800

    Add StopWatchTimer

---
+++ YameTools/Helper/StopWatchTimer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace YameTools.Helper
+{
+    public class StopWatchTimer : IDisposable
+    {
+        private readonly string _message;
+        private readonly Action<string> _logAction;
+        private readonly Stopwatch _stopwatch;
+
+        public StopWatchTimer(string message) : this(message, Console.WriteLine)
+        {
+        }
+
+        public StopWatchTimer(string message, Action<string> logAction)
+        {
+            _message = message;
+            _logAction = logAction;
+            _logAction.Invoke($"Start {_message}");
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+        }
+
+
+        public void Dispose()
+        {
+            _stopwatch.Stop();
+
+            _logAction($"Finish {_message} in {DispalyTime(_stopwatch.Elapsed)}");
+            _stopwatch.Reset();
+        }
+
+        private string DispalyTime(TimeSpan elapsed)
+        {
+            var result = String.Empty;
+            if(elapsed.Hours != 0)
+            {
+                result += $"{elapsed.Hours}h ";
+            }
+            if (elapsed.Minutes != 0)
+            {
+                result += $"{elapsed.Minutes}m ";
+            }
+            if (elapsed.Seconds != 0)
+            {
+                result += $"{elapsed.Seconds}s ";
+            }
+            if (elapsed.Milliseconds == 0)
+            {
+                result += $"{elapsed.TotalMilliseconds * 1000:n3}μs";
+            }
+            else
+            {
+                result += $"{elapsed.Milliseconds}ms ";
+            }
+            return result;
+        }
+    }
+}
---
+++ YameToolsTests/Helper/StopWatchTimerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YameTools.Helper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace YameTools.Helper.Tests
+{
+    [TestClass()]
+    public class StopWatchTimerTests
+    {
+        [TestMethod()]
+        public void StopWatchTimerTest()
+        {
+            //default is Console.WriteLine
+            using (new StopWatchTimer("Test"))
+            {
+                //Thread.Sleep(1234);
+                //show μs
+                //Thread.Sleep(new TimeSpan(1));
+            }
+        }
+    }
+}
